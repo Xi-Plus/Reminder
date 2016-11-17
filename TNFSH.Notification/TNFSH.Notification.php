008cb70a67098eb38d27448f8ac3d532eba708ce
xiplus <huangxuanyuxiplus@gmail.com>
2016-11-17 12:30:53 +0800

    strip_tags

--- TNFSH.Notification/TNFSH.Notification.php
+++ TNFSH.Notification/TNFSH.Notification.php
@@ -49,7 +49,7 @@ for ($i=0; $i < $length ; $i++) {
 	echo $message."\n";
 	if(!in_array($checktext, $list)){
 		$count++;
-		$postmessage.=$message."\n";
+		$postmessage.=strip_tags($message)."\n";
 		echo "YES\n";
 		$query=new query;
 		$query->dbname="reminder";
